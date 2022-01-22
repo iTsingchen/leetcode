4edb477aa99043e40e6df077a23ea7ab21ceacdb
anzhuc <anzhuc@keyamedical.com>
2022-01-22 15:31:33 +0800

    206. 翻转链表

---
+++ 206.反转链表.ts
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=typescript
+ *
+ * [206] 反转链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function reverseList(head: ListNode | null): ListNode | null {
+  let previous = null;
+  let current = head;
+
+  while (current) {
+    const next = current.next;
+
+    current.next = previous;
+    previous = current;
+    current = next;
+  }
+
+  return previous;
+}
+// @lc code=end
